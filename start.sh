7d0efdd2d7dcc9b132fff7ac88aa3ae3b1a82fcf
Mayank997756 <155308710+Mayank997756@users.noreply.github.com>
2024-01-04 20:03:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Mayank997756/Manku.git /Manku 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Manku 
 fi
-cd /VJ-FILTER-BOT 
+cd /Manku 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
