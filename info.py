b05eadcd3d6191b395ceb1821b38ff5abf40b01f
Mayank997756 <155308710+Mayank997756@users.noreply.github.com>
2024-02-07 21:24:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://demobotlprj.onrender.com/".format(FQDN)
+    URL = "https://new-bot-vyob.onrender.com/".format(FQDN)
 else:
-    URL = "https://demobotlprj.onrender.com/".format(FQDN)
+    URL = "https://new-bot-vyob.onrender.com/".format(FQDN)
 
 
 
